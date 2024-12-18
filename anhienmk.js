c0039f2c96c0173a20b0ccd3811505e6f0e434f8
thuan <phamtranthuan2003@gmail.com>
2024-12-18 17:49:13 +0700

    first commit

---
+++ anhienmk.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const passwordField = document.getElementById('password');
+    const togglePasswordButton = document.getElementById('hienmatkhau');
+    
+  
+    togglePasswordButton.addEventListener('click', function () {
+
+        const currentType = passwordField.type;
+        
+    
+        if (currentType === 'password') {
+            passwordField.type = 'text';
+            togglePasswordButton.textContent = 'Ẩn mật khẩu'; 
+        } else {
+            passwordField.type = 'password'; 
+            togglePasswordButton.textContent = 'Hiển thị mật khẩu';
+        }
+    });
+});
